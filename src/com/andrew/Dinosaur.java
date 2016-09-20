85b5658664d6a9510d152619dc6550afe32f8a45
Andrew <af4549fm@go.minneapolis.edu>
2016-09-20 11:29:55 -0500

    First commit created Dinosaur and DinosaurSupervisor classes tested roar and murder methods

---
+++ src/com/andrew/Dinosaur.java
@@ -0,0 +1,11 @@
+package com.andrew;
+
+class Dinosaur {
+
+    void roar() {
+        System.out.println("AAAAAAAAAAAHHHHHHHHHHHH!!!");
+    }
+    void murder() {
+        System.out.println("OM NOM NOM");
+    }
+}
---
+++ src/com/andrew/DinosaurSupervisor.java
@@ -0,0 +1,16 @@
+package com.andrew;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+
+        Dinosaur t_rex = new Dinosaur();
+        Dinosaur velociraptor = new Dinosaur();
+        Dinosaur diplodicus = new Dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+        t_rex.murder();
+
+    }
+}
